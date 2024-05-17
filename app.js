2e9ba0358d4967117e5dfb32a084f9a19e259147
Seanyao214 <sean.andrei.elgin.yao@adamson.edu.ph>
2024-05-17 12:10:13 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+﻿function calculateOrder() {
+   
+    let productName = document.getElementById('productName').value;
+    let productPrice = parseFloat(document.getElementById('productPrice').value);
+    let quantity = parseInt(document.getElementById('quantity').value);
+
+    
+    let totalPrice = productPrice * quantity;
+
+    
+    let orderSummary = `
+<h3>Your Order:</h3>
+<p>Product Name: ${productName}</p>
+<p>Price per Item: $${productPrice.toFixed(2)}</p>
+<p>Quantity: ${quantity}</p>
+<p><strong>Total Price: $${totalPrice.toFixed(2)}</strong></p>
+    `;
+
+    // Displaying the order summary.
+    document.getElementById('orderSummary').innerHTML = orderSummary;
+}
+
